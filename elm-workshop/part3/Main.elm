81d0560a004aca225853ef6748a202a67f308250
David Nimmo <dnimmo@gmail.com>
2018-07-01 10:15:54 +0100

    part 3 & 4 completed

--- elm-workshop/part3/Main.elm
+++ elm-workshop/part3/Main.elm
@@ -40,10 +40,10 @@ view model =
                 , span [ class "tagline" ] [ text "Like GitHub, but for Elm things." ]
                 ]
     in
-    div [ class "content" ]
-        [ elmHubHeader
-        , ul [ class "results" ] (List.map viewSearchResult model.results)
-        ]
+        div [ class "content" ]
+            [ elmHubHeader
+            , ul [ class "results" ] (List.map viewSearchResult model.results)
+            ]
 
 
 viewSearchResult result =
@@ -52,16 +52,28 @@ viewSearchResult result =
         , a [ href ("https://github.com/" ++ result.name), target "_blank" ]
             [ text result.name ]
         , button
-            -- TODO add an onClick handler that sends a "DELETE_BY_ID" msg
-            [ class "hide-result" ]
+            [ class "hide-result"
+            , onClick
+                { operation = "DELETE_BY_ID"
+                , data = result.id
+                }
+            ]
             [ text "X" ]
         ]
 
 
 update msg model =
-    -- TODO if msg.operation == "DELETE_BY_ID",
-    -- then return a new model without the given ID present anymore.
-    model
+    if msg.operation == "DELETE_BY_ID" then
+        { model
+            | results =
+                model.results
+                    |> List.filter
+                        (\x ->
+                            x.id /= msg.data
+                        )
+        }
+    else
+        model
 
 
 main =
